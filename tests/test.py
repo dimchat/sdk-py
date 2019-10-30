78415453570bebb0fde270baefcd8e7f01d07c05
Moky <albert.moky@gmail.com>
2019-10-31 03:39:40 +0800

    CA test OK

--- tests/test.py
+++ tests/test.py
@@ -7,10 +7,114 @@
 
     Unit test for DIM-SDK
 """
-
+import json
 import unittest
 
 from dimsdk import *
+
+from mkm.crypto.utils import base64_encode
+from mkm.immortals import Immortals
+
+immortals = Immortals()
+moki_id = immortals.identifier(string='moki@4WDfe3zZ4T7opFSi3iDAKiuTnUHjxmXekk')
+hulk_id = immortals.identifier(string='hulk@4YeVEN3aUnvC1DNUufCq1bs9zoBSJTzVEj')
+
+
+def print_data(data: CAData):
+    clazz = data.__class__.__name__
+    print('<%s>' % clazz)
+    print('    <issuer>%s</issuer>' % data.issuer)
+    print('    <validity>%s</validity>' % data.validity)
+    print('    <subject>%s</subject>' % data.subject)
+    print('    <key>%s</key>' % data.key)
+    print('</%s>' % clazz)
+
+
+def print_ca(ca: CertificateAuthority):
+    clazz = ca.__class__.__name__
+    print('<%s>' % clazz)
+    print('    <version>%d</version>' % ca.version)
+    print('    <sn>%s</sn>' % ca.sn)
+    print('    <info>%s</info>' % ca.info)
+    print('    <signature>%s</signature>' % base64_encode(ca.signature))
+    print('</%s>' % clazz)
+
+
+common = {}
+
+
+class CATestCase(unittest.TestCase):
+
+    def test1_subject(self):
+        print('\n---------------- %s' % self)
+
+        issuer = {
+            'O': 'GSP',
+            'OU': 'Service Operation Department',
+            'CN': 'dim.chat',
+        }
+
+        common['issuer'] = CASubject(issuer)
+        print('issuer: ', common['issuer'])
+        self.assertEqual(common['issuer'].organization, issuer['O'])
+
+        subject = {
+            'C': 'CN',
+            'ST': 'Guangdong',
+            'L': 'Guangzhou',
+
+            'O': 'GSP',
+            'OU': 'Service Operation Department',
+            'CN': '127.0.0.1',
+        }
+
+        common['subject'] = CASubject(subject)
+        print('subject: ', common['subject'])
+        self.assertEqual(common['subject'].organization, subject['O'])
+
+    def test2_validity(self):
+        print('\n---------------- %s' % self)
+
+        validity = {
+            'NotBefore': 123,
+            'NotAfter': 456,
+        }
+        common['validity'] = CAValidity(validity)
+        print('validity: ', common['validity'])
+
+    def test3_key(self):
+        print('\n---------------- %s' % self)
+
+        moki_meta = immortals.meta(identifier=moki_id)
+        moki_pk = moki_meta.key
+        key = moki_pk
+        common['key'] = PublicKey(key)
+        print('pubic key: ', common['key'])
+
+    def test4_ca(self):
+        print('\n---------------- %s' % self)
+
+        info = {
+            'Issuer': common['issuer'],
+            'Validity': common['validity'],
+            'Subject': common['subject'],
+            'Key': common['key'],
+        }
+        common['info'] = CAData(info)
+        print_data(common['info'])
+
+        moki = immortals.user(identifier=moki_id)
+
+        string = json.dumps(common['info']).encode('utf-8')
+        signature = moki.sign(string)
+        ca = {
+            'version': 1,
+            'sn': 1234567,
+            'info': string,
+            'signature': base64_encode(signature)
+        }
+        common['ca'] = CertificateAuthority(ca)
+        print_ca(common['ca'])
 
 
 class EntityTestCase(unittest.TestCase):
