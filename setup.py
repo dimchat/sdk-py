cdf6a7720cbf272d781089a0579b96ab4513102b
Moky <albert.moky@gmail.com>
2020-02-07 00:00:53 +0800

    mkm updated

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 
 from setuptools import setup, find_packages
 
-__version__ = '0.2.22'
+__version__ = '0.2.23'
 __author__ = 'Albert Moky'
 __contact__ = 'albert.moky@gmail.com'
 
@@ -39,8 +39,8 @@ setup(
         'Operating System :: OS Independent',
     ],
     install_requires=[
-        'dimp>=0.8.5',
-        'mkm>=0.8.5',
+        'dimp>=0.8.6',
+        'mkm>=0.8.6',
         'apns2',  # 0.4.1
         'numpy',  # 1.15.4
     ]
